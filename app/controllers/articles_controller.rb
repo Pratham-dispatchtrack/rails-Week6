5956bbac93ebd2fefdfc3bcd30bb1eb411c4babc
DT_Pratham <pratham.madhunapanthula@dispatchtrack.com>
2024-07-16 15:33:31 +0530

    added index in articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,10 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+    
+    def index
+        @all_articles = Article.all
+    end
+    
+
 end
