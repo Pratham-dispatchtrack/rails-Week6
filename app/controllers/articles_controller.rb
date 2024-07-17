c1c295c8034333e065ed46a2ea3ea5c5e681b4c0
DT_Pratham <pratham.madhunapanthula@dispatchtrack.com>
2024-07-17 15:19:23 +0530

    edit update delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,12 +9,42 @@ class ArticlesController < ApplicationController
     end
 
     def new
+        @new_article = Article.new
+    end
+
+    def edit
+        @new_article = Article.find(params[:id])
+
     end
 
     def create
         @new_article = Article.new(params.require(:article).permit(:title, :description))
-        @new_article.save
-        redirect_to article_path(@new_article)
+        if @new_article.save
+            flash[:notice] = "Article was created successfully. "
+            redirect_to article_path(@new_article)
+        else
+            render 'new', status: 422
+        end
     end
+
+   
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+        else
+            render 'edit', status: 422
+        end
+
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
+
 
 end
