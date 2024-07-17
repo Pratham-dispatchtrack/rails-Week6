8bcb667dda6e08578425a7f56c8ea6b725e544fc
DT_Pratham <pratham.madhunapanthula@dispatchtrack.com>
2024-07-17 10:18:32 +0530

    added new, create to controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,14 @@ class ArticlesController < ApplicationController
     def index
         @all_articles = Article.all
     end
-    
+
+    def new
+    end
+
+    def create
+        @new_article = Article.new(params.require(:article).permit(:title, :description))
+        @new_article.save
+        redirect_to article_path(@new_article)
+    end
 
 end
